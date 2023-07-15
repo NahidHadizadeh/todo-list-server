7dd9c2da8c1025ac44e3aa186a0ae2fbf3f835cd
NahidHadizadeh <nahidhadizadeh7@gmail.com>
2023-07-15 15:25:55 +0330

    handle bug & add "deleteHistory" & delete comments

--- controller/historyController.js
+++ controller/historyController.js
@@ -1,9 +1,9 @@
-const membersModal = require("../model/historyModel");
+const historyModel = require("../model/historyModel");
 
 const createNewHistory = async (req, res) => {
   try {
     const newHistory = req.body;
-    const history = await membersModal.create(newHistory);
+    const history = await historyModel.create(newHistory);
     return res.status(200).json(history);
   } catch (err) {
     return res.status(500).json({ msg: { err } });
@@ -14,55 +14,25 @@ const createNewHistory = async (req, res) => {
 
 const getAllHistorys = async (req, res) => {
   try {
-    const AllHistory = await membersModal.find({});
+    const AllHistory = await historyModel.find({});
     return res.status(200).json(AllHistory);
   } catch (error) {
     res.status(500).json({ msg: error });
   }
 };
 
-// const getOneMember = async (req, res) => {
-//   try {
-//     const { id } = req.params;
-//     const members = await membersModal.findOne({ _id: id });
-//     return res.status(200).json(members);
-//   } catch (error) {
-//     res.status(500).json({ msg: error });
-//   }
-// };
-
-// const deleteTodo = async (req, res) => {
-//   try {
-//     const { id } = req.params;
-//     const members = await membersModal.findOneAndDelete({ _id: id });
-//     return res.status(200).json(members);
-//   } catch (error) {
-//     res.status(500).json({ msg: error });
-//   }
-// };
-
-// const updateMember = async (req, res) => {
-//   try {
-//     const { id: _id } = req.params;
-//     const newCourseData = req.body;
-
-//     const members = await membersModal.findByIdAndUpdate(
-//       _id,
-//       { ...newCourseData, _id },
-//       { new: true }
-//     );
-//     return res.status(200).json(members);
-//   } catch (error) {
-//     res.status(500).json({ msg: error });
-//   }
-// };
+const deleteHistory = async (req, res) => {
+  try {
+    // const { id } = req.params;
+    const history = await historyModel.deleteMany({});
+    return res.status(200).json(history);
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
+};
 
 module.exports = {
   getAllHistorys,
   createNewHistory,
-  //   getOneMember,
-  //   createNewMember,
-  //   updateMember,
-  //   deleteTodo,
-  //   updateTodo,
+  deleteHistory,
 };
